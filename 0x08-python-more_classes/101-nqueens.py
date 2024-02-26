89a044dbac5bf345d58a79faa3bde4f5405fb88c
ammar <ammar@haddad.com>
2024-02-26 14:20:02 +0100

    done

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,20 +1,43 @@
 #!/usr/bin/python3
-
 import sys
-if len(sys.argv) == 1:
-    print("Usage: nqueens N")
-    exit(1)
-if not sys.argv[1].isdigit():
-    print("N must be a number")
-    exit(1)
-if int(sys.argv[1]) < 4:
-    print("N must be at least 4")
-    exit(1)
-if int(sys.argv[1]) == 4:
-    print("[[0, 1], [1, 3], [2, 0], [3, 2]]")
-    print("[[0, 2], [1, 0], [2, 3], [3, 1]]")
-if int(sys.argv[1]) == 6:
-    print("[[0, 1], [1, 3], [2, 5], [3, 0], [4, 2], [5, 4]]")
-    print("[[0, 2], [1, 5], [2, 1], [3, 4], [4, 0], [5, 3]]")
-    print("[[0, 3], [1, 0], [2, 4], [3, 1], [4, 5], [5, 2]]")
-    print("[[0, 4], [1, 2], [2, 0], [3, 5], [4, 3], [5, 1]]")
+
+
+def is_valid(board, row, col):
+    for r, c in board:
+        if c == col or r + c == row + col or r - c == row - col:
+            return False
+    return True
+
+
+def nqueens_helper(n, row, board, solutions):
+    if row == n:
+        solutions.append(board.copy())
+    else:
+        for col in range(n):
+            if is_valid(board, row, col):
+                board.append((row, col))
+                nqueens_helper(n, row + 1, board, solutions)
+                board.pop()
+
+
+def nqueens(n):
+    if not isinstance(n, int):
+        print("N must be a number")
+        sys.exit(1)
+    if n < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+    solutions = []
+    nqueens_helper(n, 0, [], solutions)
+    for solution in solutions:
+        print(solution)
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+    try:
+        n = int(sys.argv[1])
+        nqueens(n)
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
